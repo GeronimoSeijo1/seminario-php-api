955ac50a1772cc048989aee240fb373a7e788446
Thiago372 <nicolasvillagran321@gmail.com>
2025-06-26 20:50:26 -0300

    Página addMazoPage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import StatPage from './pages/stat/statPage';
+import AddMazoPage from './pages/mazo/addMazoPage';
 // (más adelante se agregara RegistroPage, LoginPage, etc.)
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<StatPage />} />
+      <Route path="/addMazoPage" element={<AddMazoPage />} />
       {/* Ejemplo de futuras rutas */}
       {/* <Route path="/registro" element={<RegistroPage />} /> */}
       {/* <Route path="/login" element={<LoginPage />} /> */}
--- frontend/src/services/apiServices.js
+++ frontend/src/services/apiServices.js
@@ -1,3 +1,14 @@
 import { api } from '../api/api';
 
 export const getStats = () => api.get('/estadisticas');
+export const getMazos = (id) => api.get('/usuarios/${id:[0-9]+}/mazos', {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  });
+export const getCartas = (filtros = {}) => api.get('/cartas', { params: filtros });
+export const addMazo = (datos) => api.post('/mazos',datos, {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  });
